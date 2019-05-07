b9647bffd6eed53bd1e2203f9b08cba3671bebcf
Anup DebBarma <sunnyoranup@gmail.com>
2019-05-07 06:56:54 +0530

    warnings removed

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,8 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import { FaGulp } from "react-icons/fa"
+
 import SEO from "../components/seo"
 import BackgroundSection from "../components/Globals/BackgroundSection"
 import Info from "../components/Home/Info"
